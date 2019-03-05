417d76828bb1a717407b45567ed6ba551b931d4f
rajesh <upadhyay.rajesh@rediffmail.com>
2019-03-05 18:02:08 +0530

    d

---
+++ src/day1to4/redux thung/src/api/CourseApi.js
@@ -0,0 +1,108 @@
+import delay from './delay';
+
+// This file mocks a web API by working with the hard-coded data below.
+// It uses setTimeout to simulate the delay of an AJAX call.
+// All calls return promises.
+const courses = [
+    {
+        id: "react-flux-building-applications",
+        title: "Building Applications in React and Flux",
+        authorId: "Rajesh Upadhyay",
+        length: "5:08",
+        category: "JavaScript"
+    },
+    {
+        id: "clean-code",
+        title: "Clean Code: Writing Code for Humans",
+        authorId: "Ashutosh Asthana",
+        length: "3:10",
+        category: "Software Practices"
+    },
+    {
+        id: "architecture",
+        title: "Architecting Applications for the Real World",
+        authorId: "Venkatesh",
+        length: "2:52",
+        category: "Software Architecture"
+    },
+    {
+        id: "career-reboot-for-developer-mind",
+        title: "Becoming an Outlier: Reprogramming the Developer Mind",
+        authorId: "Brij Mohan",
+        length: "2:30",
+        category: "Career"
+    }
+    
+];
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
+
+//This would be performed on the server in a real app. Just stubbing in.
+const generateId = (course) => {
+    return replaceAll(course.title, ' ', '-');
+};
+
+class CourseApi {
+    static getAllCourses() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(Object.assign([], courses));
+            }, delay);
+        });
+    }
+
+    static saveCourse(course) {
+        course = Object.assign({}, course); // to avoid manipulating object passed in.
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                // Simulate server-side validation
+                const minCourseTitleLength = 1;
+                if (course.title.length < minCourseTitleLength) {
+                    reject(`Title must be at least ${minCourseTitleLength} characters.`);
+                }
+
+                if (course.id) {
+                    const existingCourseIndex = courses.findIndex(a => a.id === course.id);
+                    courses.splice(existingCourseIndex, 1, course);
+                } else {
+                    //Just simulating creation here.
+                    //The server would generate ids and watchHref's for new courses in a real app.
+                    //Cloning so copy returned is passed by value rather than by reference.
+                    course.id = generateId(course);
+                    courses.push(course);
+                }
+
+                resolve(course);
+            }, delay);
+        });
+    }
+
+    static deleteCourse(courseId) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const indexOfCourseToDelete = courses.findIndex(course => course.id === courseId);
+                courses.splice(indexOfCourseToDelete, 1);
+                resolve();
+            }, delay);
+        });
+    }
+
+
+    static getCourse(courseId) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const existingCourseIndex = courses.findIndex(course => course.id === courseId);
+                
+                const courseFound = Object.assign({}, courses[existingCourseIndex]);
+
+                resolve(courseFound);
+
+            }, delay);
+        });
+    }
+
+}
+
+export default CourseApi;
---
+++ src/day1to4/redux thung/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const About = () => {
+  return (
+    <div className="container">
+      <h1>About</h1>
+      <p>This is RKCP INFORMATION TECHNOLOGIES owned by Mr. Rajesh Upadhyay</p>
+    </div>
+  );
+};
+
+
+export default About;
